a9a1e78f4a5e41df7ae4d276e490db7f1952e479
Andrew Dunai <andunai@gmail.com>
2017-09-12 18:24:01 +0300

    Site

--- dunai/site/views.py
+++ dunai/site/views.py
@@ -1,6 +1,15 @@
+import os
+import yaml
+
 from django.shortcuts import render
+from django.conf import settings
 
 
 def index(request):
-    return render(request, 'index.html')
+    f = open(os.path.join(settings.BASE_DIR, 'dunai', 'files', 'cv.yaml'))
+    with f:
+        cv = yaml.load(f.read())
+    return render(request, 'index.html', dict(
+        cv=cv
+    ))
 
