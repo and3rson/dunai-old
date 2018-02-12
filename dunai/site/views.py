5e67b016bc5ab029616d5dc5969d365c4b7b640c
Andrew Dunai <andrew@dun.ai>
2018-02-12 14:42:58 +0200

    Stuff

--- dunai/site/views.py
+++ dunai/site/views.py
@@ -5,7 +5,8 @@ import yaml
 import docx
 from docx.enum.text import WD_ALIGN_PARAGRAPH  # pylint: disable=no-name-in-module
 from docx.enum.section import WD_SECTION  # pylint: disable=no-name-in-module
-# from docx.enum.style import WD_STYLE_TYPE  # pylint: disable=no-name-in-module
+from docx.enum.style import WD_STYLE_TYPE  # pylint: disable=no-name-in-module
+from docx.shared import Pt, RGBColor
 from django.urls import reverse
 from django.shortcuts import render
 from django.conf import settings
@@ -57,41 +58,49 @@ def cv_doc(request):
     cv = get_cv()
 
     doc = docx.Document()
-    for style in doc.styles:
-        print(style)
     doc.styles['Normal'].font.name = 'Arial'
-    print(dir(doc.styles['Heading 1']))
-    # date_style = doc.styles.add_style('Date', WD_STYLE_TYPE.CHARACTER)
+    doc.styles['Heading 3'].paragraph_format.space_after = Pt(32)
+    date_style = doc.styles.add_style('Date', WD_STYLE_TYPE.CHARACTER)
+    date_style.font.color.rgb = RGBColor(192, 192, 216)
+    text_par = doc.styles.add_style('Text paragraph', WD_STYLE_TYPE.PARAGRAPH)
+    text_par.font.name = 'Arial'
+    text_par.paragraph_format.space_before = Pt(8)
+    text_par.paragraph_format.first_line_indent = Pt(16)
     h1 = doc.add_heading('Andrew Dunai', 0)
     h1.alignment = WD_ALIGN_PARAGRAPH.CENTER
     h2 = doc.add_heading('Full Stack + Software Architect', 1)
     h2.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
-    doc.add_heading('Bio', 1)
+    doc.add_heading('About', 1)
     for par_text in cv['bio']:
-        doc.add_paragraph(par_text.strip())
+        par = doc.add_paragraph(par_text.strip())
+        par.style = text_par
 
-    doc.add_heading('Companies', 1)
+    doc.add_heading('Experience', 1)
     for company in cv['companies']:
         doc.add_section(WD_SECTION.CONTINUOUS)
-        doc.add_heading(company['role'], 2)
-        head = doc.add_heading(company['name'], 3)
+        head = doc.add_heading(company['role'] + '\n', 2)
+        run = head.add_run(company['name'])
         run = head.add_run(' ({} - {})'.format(
-            company['start'],
-            company['end'] if 'end' in company else 'now'
+            company['start'].strftime('%b %Y'),
+            company['end'].strftime('%b %Y') if 'end' in company else 'now'
         ))
         run.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+        run.style = date_style
         run.italic = True
 
         desc = doc.add_paragraph('')
         desc.add_run('About: ').bold = True
         desc.add_run(company['info'].strip())
+        desc.style = text_par
 
         desc = doc.add_paragraph('')
         desc.add_run('My role: ').bold = True
         desc.add_run(company['description'].strip())
+        desc.style = text_par
 
         tech = doc.add_paragraph('')
+        tech.style = text_par
         tech.add_run('Technologies: ').bold = True
         for i, technology in enumerate(company['technologies']):
             if i > 0:
